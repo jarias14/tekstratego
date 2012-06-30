0c4cd49649322ee4955c025832a69bf61484cdd3
Jorge <jarias14@gmail.com>
2012-06-30 12:26:44 -0400

    added factory pattern for building indicators at service layer

--- src/main/java/com/jarias14/tekstratego/service/pricer/rest/RestPricerService.java
+++ src/main/java/com/jarias14/tekstratego/service/pricer/rest/RestPricerService.java
@@ -1,25 +1,51 @@
 package com.jarias14.tekstratego.service.pricer.rest;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.jarias14.tekstratego.service.pricer.rest.resource.IndicatorResource;
 import com.jarias14.tekstratego.service.thinker.rest.resource.BaseResource;
 
 @Service("restPricerService")
 public interface RestPricerService {
     
     /**
-     * Gets the list of prices for the given stock and indicator.
+     * Creates an indicator given the stock and the body.
      * @return the list of prices for the request.
      */
     @POST
-    @Path("/prices/{stock-id}/indicator/{indicator-id}")
+    @Path("/indicators")
     @Produces({"application/json"})
     @Consumes({"application/json"})
-    public BaseResource getPrices();
+    public BaseResource createIndicator(@RequestBody IndicatorResource indicator);
+    
+    /**
+     * Creates an indicator given the stock and the body.
+     * @return the list of prices for the request.
+     */
+    @GET
+    @Path("/indicators/{indicator-id}")
+    @Produces({"application/json"})
+    @Consumes({"application/json"})
+    public BaseResource getIndicator(@PathParam("indicator-id") String indicatorId);
+    
+    /**
+     * Gets the list of prices for the given stock and indicator.
+     * @return the list of prices for the request.
+     */
+    @GET
+    @Path("/indicators/{indicator-id}/prices/{stock-id}")
+    @Produces({"application/json"})
+    @Consumes({"application/json"})
+    public BaseResource getValues(@PathParam("indicator-id") String indicatorId, @PathParam("stock-id") String stockId,
+            @QueryParam("sizeOfBars") String sizeOfBars, @QueryParam("startDate") String startDate, @QueryParam("numberOfBars") String numberOfBars);
     
 }
