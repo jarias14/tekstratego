593c653b593f19fa2880b1c9cc77948a2f61597c
Jorge <jarias14@gmail.com>
2012-06-30 17:24:19 -0400

    implemented membase connector

--- src/main/java/com/jarias14/tekstratego/common/utilities/MembaseConnector.java
+++ src/main/java/com/jarias14/tekstratego/common/utilities/MembaseConnector.java
@@ -1,48 +1,25 @@
 package com.jarias14.tekstratego.common.utilities;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import net.spy.memcached.AddrUtil;
 import net.spy.memcached.MemcachedClient;
 
 public class MembaseConnector {
     
-    List<String> hosts;
     MemcachedClient client;
-    
-    public MembaseConnector() {
-        hosts = new ArrayList<String>();
-        hosts.add("localhost:11211");
-        
-        List<InetSocketAddress> address = AddrUtil.getAddresses(hosts);
-        
-        try {
-            client = new MemcachedClient(address);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+
+    public void save(String key, Object value, int timeToLiveInSeconds) {
+        client.set(key, timeToLiveInSeconds, value);
     }
     
-    public void save(String key, Object value) {
-        
-        Object spoon = client.get("spoon");
-        if (spoon == null) {
-            System.out.println("There is no spoon.");
-            client.set("spoon", 10, "Hello World!");
-        } else {
-            System.out.println((String)spoon);
-        }
-        client.waitForQueues(1, TimeUnit.MINUTES);
-        System.exit(0);
+    public Object read(String key) {
+        return client.get(key);
     }
     
-    public void read(String key) {
-        
+    public MemcachedClient getClient() {
+        return client;
+    }
+
+    public void setClient(MemcachedClient client) {
+        this.client = client;
     }
 
 }
--- src/test/java/com/jarias14/tekstratego/common/utilities/MembaseConnectorTest.java
+++ src/test/java/com/jarias14/tekstratego/common/utilities/MembaseConnectorTest.java
@@ -1,13 +1,30 @@
 package com.jarias14.tekstratego.common.utilities;
 
-import org.junit.Test;
+import javax.annotation.Resource;
 
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:membase.xml")
 public class MembaseConnectorTest {
 
+    @Resource(name="membaseConnector")
+    MembaseConnector mc;
+    
     @Test
     public void test() {
-        MembaseConnector mc = new MembaseConnector();
-        mc.save("a", "b");
+        
+        String key = "a";
+        String value = "b";
+        int ttl = 10;
+        
+        mc.save(key, value, ttl);
+        Assert.assertEquals(value, mc.read(key));
     }
 
 }
