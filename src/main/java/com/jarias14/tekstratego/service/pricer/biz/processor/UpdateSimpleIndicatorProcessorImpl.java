10728d05869c4dea1496f5a44be77b007ebce7b9
unknown <jarias14@gmail.com>
2015-04-05 11:58:32 -0400

    starting with the update simple indicator proessor

--- src/main/java/com/jarias14/tekstratego/service/pricer/biz/processor/UpdateSimpleIndicatorProcessorImpl.java
+++ src/main/java/com/jarias14/tekstratego/service/pricer/biz/processor/UpdateSimpleIndicatorProcessorImpl.java
@@ -1,16 +1,28 @@
 package com.jarias14.tekstratego.service.pricer.biz.processor;
 
-import com.jarias14.tekstratego.common.models.DataPointDescription;
+import com.jarias14.tekstratego.common.models.DataPointCollection;
+import com.jarias14.tekstratego.common.models.DataPointIndicator;
+import com.jarias14.tekstratego.common.models.Stock;
 import com.jarias14.tekstratego.common.skeleton.Processor;
+
+import java.util.Map;
 
 /**
  * Created by jarias14 on 4/4/2015.
  */
-public class UpdateSimpleIndicatorProcessorImpl implements Processor<DataPointDescription> {
+public class UpdateSimpleIndicatorProcessorImpl implements Processor<UpdateSimpleIndicatorRequest> {
+
+
 
     @Override
-    public void process(DataPointDescription dataPointDescription) {
+    public void process(UpdateSimpleIndicatorRequest updateSimpleIndicatorRequest) {
 
+        long time = updateSimpleIndicatorRequest.getRequestedIndicator().getTime();
+        Stock stock = updateSimpleIndicatorRequest.getRequestedIndicator().getStock();
+        DataPointIndicator indicator = updateSimpleIndicatorRequest.getRequestedIndicator().getDetails().getIndicator();
+        Map<DataPointIndicator, DataPointCollection> rawDataPoints = updateSimpleIndicatorRequest.getData();
+
+        
 
 
         return;
