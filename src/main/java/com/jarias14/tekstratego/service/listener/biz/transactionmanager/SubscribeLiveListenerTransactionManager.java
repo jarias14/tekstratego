f965b503f1b507784ae9e78ff195db175e83dca8
unknown <jarias14@gmail.com>
2015-04-14 14:23:10 -0400

    IB only supports data in intervals of 5s

--- src/main/java/com/jarias14/tekstratego/service/listener/biz/transactionmanager/SubscribeLiveListenerTransactionManager.java
+++ src/main/java/com/jarias14/tekstratego/service/listener/biz/transactionmanager/SubscribeLiveListenerTransactionManager.java
@@ -1,9 +1,12 @@
 package com.jarias14.tekstratego.service.listener.biz.transactionmanager;
 
+import com.jarias14.tekstratego.common.models.DataPointSize;
 import com.jarias14.tekstratego.common.skeleton.DataAccessObject;
 import com.jarias14.tekstratego.common.skeleton.TransactionManager;
 import com.jarias14.tekstratego.service.listener.models.RawDataRequest;
 import com.jarias14.tekstratego.service.listener.models.RawDataResponse;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by jarias14 on 4/4/2015.
@@ -14,6 +17,10 @@ public class SubscribeLiveListenerTransactionManager implements TransactionManag
 
     @Override
     public RawDataResponse process(RawDataRequest request) {
+
+        // IB only supports 5 second live data
+        request.setDataPointSize(new DataPointSize(TimeUnit.SECONDS, 5));
+
         return new RawDataResponse(request, liveDataDao.request(request));
     }
 
